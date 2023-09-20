519fbeeb3c8deae222c54139520f33f4f5ea3dd5
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-09-20 13:58:52 -0400

    preparation for alien drawing

--- Assignments/02-draw-an-alien/js/script.js
+++ Assignments/02-draw-an-alien/js/script.js
@@ -28,19 +28,5 @@ function setup() {
  * Description of draw()
 */
 function draw() {
-    createCanvas(300, 600);
-    background(255, 180, 0);
-
-    //square
-    strokeWeight(5);
-    stroke(0, 0, 200);
-    fill(mouseX, mouseY, random(0, 255), random(0, 255));
-    rectMode(CENTER);
-    rect(150, 300, 80, 80);
-    rect(150, 300, 50, 50);
-
-   
-
-
 
 }
