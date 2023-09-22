78035af256523eea34a64353bdcd63c7550cf93b
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-09-22 17:16:38 -0400

    Been Doing Lessons (Changing Variables etc.)

--- Experiments/template-p5-project/js/script.js
+++ Experiments/template-p5-project/js/script.js
@@ -8,25 +8,32 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
+
 function preload() {
 
 }
 
+let backgroundShade = 0;
+let circleX = 0;
+let circleY = 250;
+let circleSize = 200;
+let circleSpeed = 2;
+let circleAcceleration = 0.25;
 
-/**
- * Description of setup
-*/
+
 function setup() {
+
+    createCanvas(1000, 500);
 
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
+
+    circleX+= circleSpeed;
+    circleSpeed = circleSpeed + circleAcceleration;
+
+    background(backgroundShade);
+    ellipse(circleX, circleY, circleSize);
 
 }
