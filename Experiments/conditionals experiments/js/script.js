f4c5289be481658794b0a9139789c313e353e699
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-09-27 18:41:06 -0400

    Made a bunch of changes

--- Experiments/conditionals experiments/js/script.js
+++ Experiments/conditionals experiments/js/script.js
@@ -1,32 +1,30 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
-
-"use strict";
-
-/**
- * Description of preload
-*/
-function preload() {
-
+let backgroundShade = 0;
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 2
 }
 
 
-/**
- * Description of setup
-*/
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
-/**
- * Description of draw()
-*/
-function draw() {
 
+function draw() {
+    background(backgroundShade);
+
+    circle.x = circle.x + circle.speed;
+
+    if (circle.x > width) {
+        circle.speed = -circle.speed;
+    }
+
+    if (circle.x < 0) {
+        circle.speed = -circle.speed;
+    }
+
+    ellipse(circle.x, circle.y, circle.size);
 }
