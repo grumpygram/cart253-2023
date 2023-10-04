e4379c3302ab27ff5c289d6d5cc9ae25c658ad7c
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-10-03 23:25:09 -0400

    Have done setup

--- Assignments/03-i-like-to-move-it/js/script.js
+++ Assignments/03-i-like-to-move-it/js/script.js
@@ -1,32 +1,70 @@
 /**
- * Title of Project
- * Author Name
+ * I like to move it
+ * Graeme Peters
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+ * I am making something that will move, with bla bla bla.
 
 "use strict";
 
 /**
- * Description of preload
+ * Making Javascript objects
 */
+
+
+//Objects
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+
+};
+
+let circle1 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    fill: 255,
+    alpha: 200
+};
+
+let circle2 = {
+    x: 500,
+    y: 250,
+    size: 75,
+    fill: 255,
+    alpha: 200
+};
+
 function preload() {
 
 }
 
 
-/**
- * Description of setup
-*/
+//Setup
 function setup() {
 
+    createCanvas(500,500);
+    noStroke();
 }
 
 
-/**
- * Description of draw()
-*/
+//Drawing
 function draw() {
 
-}
+    //Background
+    background(bg.r,bg.g,bg.b);
+    bg.r = bg.r + 1;
+
+    //Drawing Circle1
+    circle1.x = circle1.x + 1;
+    circle1.x = constrain(circle1.x,0,width/2);
+    fill(circle1.fill,circle1.alpha);
+    ellipse(circle1.x,circle1.y,circle1.size);
+
+    //Drawing Circle2
+    circle2.x = circle2.x - 1;
+    circle2.x = constrain(circle2.x,width/2,width);
+    fill(circle2.fill,circle2.alpha);
+    ellipse(circle2.x,circle2.y,circle2.size);
+ 
+} 
