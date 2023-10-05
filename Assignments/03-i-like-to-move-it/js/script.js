ea846a8a5268debdb4c1e077b7d4fb58eafd5566
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-10-04 23:03:07 -0400

    Finished Project 3

--- Assignments/03-i-like-to-move-it/js/script.js
+++ Assignments/03-i-like-to-move-it/js/script.js
@@ -23,6 +23,8 @@ let circle1 = {
     x: 0,
     y: 250,
     size: 100,
+    growthRate: 1,
+    speed: 1,
     fill: 255,
     alpha: 200
 };
@@ -31,6 +33,8 @@ let circle2 = {
     x: 500,
     y: 250,
     size: 75,
+    sizeRatio: 0.5,
+    speed: -1,
     fill: 255,
     alpha: 200
 };
@@ -43,7 +47,7 @@ function preload() {
 //Setup
 function setup() {
 
-    createCanvas(500,500);
+    createCanvas(500, 500);
     noStroke();
 }
 
@@ -52,19 +56,22 @@ function setup() {
 function draw() {
 
     //Background
-    background(bg.r,bg.g,bg.b);
-    bg.r = bg.r + 1;
+    background(bg.r, bg.g, bg.b);
+    bg.r = map(circle1.size, 100, width/4*3, 0, 255);
 
     //Drawing Circle1
-    circle1.x = circle1.x + 1;
-    circle1.x = constrain(circle1.x,0,width/2);
-    fill(circle1.fill,circle1.alpha);
-    ellipse(circle1.x,circle1.y,circle1.size);
+    circle1.x = circle1.x + circle1.speed;
+    circle1.x = constrain(circle1.x, 0, width/2);
+    circle1.size = circle1.size + circle1.growthRate;
+    circle1.size = constrain(circle1.size, 0, width/4*3);
+    fill(circle1.fill, circle1.alpha);
+    ellipse(circle1.x, circle1.y, circle1.size);
 
     //Drawing Circle2
-    circle2.x = circle2.x - 1;
-    circle2.x = constrain(circle2.x,width/2,width);
-    fill(circle2.fill,circle2.alpha);
-    ellipse(circle2.x,circle2.y,circle2.size);
+    circle2.x = circle2.x + circle2.speed;
+    circle2.x = constrain(circle2.x, width/2, width);
+    circle2.size = circle1.size * circle2.sizeRatio;
+    fill(circle2.fill, circle2.alpha);
+    ellipse(circle2.x, circle2.y, circle2.size);
  
 } 
