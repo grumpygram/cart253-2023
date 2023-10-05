3e3d52feb9b93e7110289b722092b81a5a1fe1d0
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-10-05 17:15:42 -0400

    Finished videos 4.1 & 4.2
    Learned about if/then statements and signs such as < (lesser than), > (greater than), && (and), || (or), and ! (not).

--- Experiments/04-conditionals/js/script.js
+++ Experiments/04-conditionals/js/script.js
@@ -1,32 +1,36 @@
 /**
- * Title of Project
- * Author Name
+ * Learning Conditionals
+ * Graeme Peters
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * This is my file to learn to use conditionals by following along with Pippin's 
+ * videos.
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let backgroundShade = 0;
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 1.5,
+};
 
-}
-
-
-/**
- * Description of setup
-*/
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
+    background(backgroundShade);
+    
+    circle.x = circle.x + circle.speed;
 
+    fill(255);
+
+    if (!(circle.x < width/3)) {
+            fill(255, 0, 0);
+        }
+
+    ellipse(circle.x, circle.y, circle.size);
 }
