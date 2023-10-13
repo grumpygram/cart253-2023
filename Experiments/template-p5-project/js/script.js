8775db918077e0be381066453aafd85fe8a5c2db
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-10-13 12:30:01 -0700

    Finished 5.4 and 5.5
    I learned about text styles and states, as well as switching between states using functions

--- Experiments/template-p5-project/js/script.js
+++ Experiments/template-p5-project/js/script.js
@@ -8,30 +8,62 @@
 
 "use strict";
 
+let circle = {
+    x: 0,
+    y: 250,
+    vx: 2,
+    vy: 0,
+    size: 80
+};
 
-function preload() {
-
-}
+let state = `title`;
 
 function setup() {
     createCanvas(500, 500);
-
-    let hotCelsius = toCelsius(100);
-    console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`);
-
-    let coldCelsius = toCelsius(10);
-    console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
-
+    textSize(64);
+    textAlign(CENTER, CENTER);
 }
-
 
 function draw() {
     background(0);
 
+    if(state === `title`) {
+    title();
+    }
+    else if (state === `animation`) {
+    animation();
+    }
+    else if (state === `ending`) {
+    ending();
+    }
 }
 
-function toCelsius(fahrenheit) {
-    let celsius = (fahrenheit - 32) * 5/9;
-    return celsius;
-    
+function title() {
+    //TITLE
+    fill(255);
+    text(`Life.`, width/2, height/2);
+}
+
+function animation() {
+    //ANIMATION
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    if (circle.x > width) {
+        state = `ending`;
+    }
+
+    ellipse(circle.x, circle.y, circle.size);
+}
+
+function ending() {
+    //ENDING
+    fill(127);
+    text(`The End.`, width/2, height/2);
+}
+
+function keyPressed() {
+    if (state === `title`) {
+    state = `animation`;
+    }
 }
