3abec70e15679990222094636e2c5031a6e4804a
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-10-13 15:00:06 -0700

    finished 5.6 and 5.7
    I learned about text and letter codes and such, as well as about random numbers for automated movement

--- Experiments/template-p5-project/js/script.js
+++ Experiments/template-p5-project/js/script.js
@@ -8,62 +8,10 @@
 
 "use strict";
 
-let circle = {
-    x: 0,
-    y: 250,
-    vx: 2,
-    vy: 0,
-    size: 80
-};
-
-let state = `title`;
-
 function setup() {
-    createCanvas(500, 500);
-    textSize(64);
-    textAlign(CENTER, CENTER);
+
 }
 
 function draw() {
-    background(0);
 
-    if(state === `title`) {
-    title();
-    }
-    else if (state === `animation`) {
-    animation();
-    }
-    else if (state === `ending`) {
-    ending();
-    }
-}
-
-function title() {
-    //TITLE
-    fill(255);
-    text(`Life.`, width/2, height/2);
-}
-
-function animation() {
-    //ANIMATION
-    circle.x = circle.x + circle.vx;
-    circle.y = circle.y + circle.vy;
-
-    if (circle.x > width) {
-        state = `ending`;
-    }
-
-    ellipse(circle.x, circle.y, circle.size);
-}
-
-function ending() {
-    //ENDING
-    fill(127);
-    text(`The End.`, width/2, height/2);
-}
-
-function keyPressed() {
-    if (state === `title`) {
-    state = `animation`;
-    }
 }
