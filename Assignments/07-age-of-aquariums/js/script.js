6aeb571fbfdf0d7972382bf0a7b13b405e3e0205
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-11-09 20:29:05 -0500

    Finished the Arrays videos.
    I will use this file for Age of Aquariums once I have finished the object oriented programming videos and the project 2 proposal

--- Assignments/07-age-of-aquariums/js/script.js
+++ Assignments/07-age-of-aquariums/js/script.js
@@ -8,76 +8,42 @@
 
 "use strict";
 
-// Our fish
+/*let fortunes = [
+    `nice hair!`,
+    `your bike will slide in the snow`,
+    `happiness is not promised; create it yourself`,
+    `life is better when shared with friends`
+    `je t'aime <3 xoxo`
+];
+*/
 
-let school = [];
-let schoolSize = 1;
+let images = [1.5,1.75,2.25,2.75,3];
+let displayImage;
+
+function preload() {
+    images[0] = loadImage(`assets/images/clown-0.png`);
+    images[1] = loadImage(`assets/images/clown-1.png`);
+    images[2] = loadImage(`assets/images/clown-2.png`);
+    images[3] = loadImage(`assets/images/clown-3.png`);
+    images[4] = loadImage(`assets/images/clown-4.png`);
+    images[5] = loadImage(`assets/images/clown-5.png`);
+    images[6] = loadImage(`assets/images/clown-6.png`);
+    images[7] = loadImage(`assets/images/clown-7.png`);
+    images[8] = loadImage(`assets/images/clown-8.png`);
+    images[9] = loadImage(`assets/images/clown-9.png`);
+}
 
 function setup() {
-  createCanvas(600, 600);
+    createCanvas(600, 600);
 
-  for (let i = 0; i < schoolSize; i++) {
-    let fish = createFish(random(0, width), random(0, height));
-    school.push(fish);
-  }
-
+    displayImage = random(images);
 }
 
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2
-  };
-  return fish;
-}
-
-// draw()
-// Moves and displays our fish
 function draw() {
-  background(0);
+    background(0);
 
-  for (let i = 0; i < school.length; i++) {
-    moveFish(school[i]);
-    displayFish(school[i]);
-  }
-}
-
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
-  }
-
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
-
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
-}
-
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
-}
-
-function mousePressed() {
-    let fish = createFish(mouseX, mouseY);
-    school.push(fish);
+    push();
+    imageMode(CENTER);
+    image(displayImage, width/2, height/2);
+    pop();
 }
