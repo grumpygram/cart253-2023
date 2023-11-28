5b2a7c73a9e34fa2c02991926d14571b53f8ddf9
grumpygram <144966696+grumpygram@users.noreply.github.com>
2023-11-28 17:37:03 -0500

    Making a more fun game for the final
    It's football!

--- Assignments/project-02/js/script.js
+++ Assignments/project-02/js/script.js
@@ -1,83 +1,152 @@
 /**
- * Bike Fast
+ * Throw Hard
  * Graeme Peters
  * 
- * This is a game where you control a little cyclist who moves
- * faster depending on how quickly you click.
+ * Game where you choose the angle and power of a ball, then your players catch it and score a touchdown
  */
 
 "use strict";
 
-let badGuys = [];
+let state = `powerSelector`;
 
-let userRacer;
-let lapsCompleted = 0;
-let button;
-
-let rivalsRules = {
-    numRivals: 2
+//Power stuff
+let powerValue;
+let powerBar = {
+    x: 600,
+    y: 150,
+    width: 40,
+    height: 100,
+    minHeight: 0,
+    maxHeight: 400,
+    fill: {
+        r: 255,
+        g: 220,
+        b: 0
+    },
+    heightChange: 5
 }
 
+//Angle stuff
+let angleValue;
 
+let angleArrow = {
+    centerX: 600,
+    centerY: 400,
+    length: 200,
+    angle: 180,
+    angleChange: 1,
+    centerSize: 20,
+    fill: {
+        r: 255,
+        g: 0,
+        b: 0
+    },
+    maxAngle: 230,
+    minAngle: 130
+}
+
+//Setup
 function setup() {
-    createCanvas(600, 600);
-
-    for (let i = 0; i < rivalsRules.numRivals; i++) {
-        badGuys.push(new Rival(random(0, 255), random(0, 255), random(0, 255)));
-    }
-
-    userRacer = new UserRacer();
-
-//Making button to control speed
-    button = createButton(`Click to pedal faster`)
-    button.position(width/2, height/2);
-    button.mousePressed(userRacer.accelerate());
+    createCanvas(700, 700);
 
 }
 
 
 function draw() {
-    background(0);
+    background(0, 200, 0);
 
-    for (let i = 0; i < rivalsRules.numRivals; i++) {
-        let rival = badGuys[i];
-
-//Displayin
-    rival.display();
-    userRacer.display();
-
-//Moving the rivals
-    rival.move();
-
-// Moving the user
-    userRacer.move();
-
-//Adding laps
-    if (rival.lapDone() && userRacer.lapDone()) {
-        lapsCompleted++;
+    if (state === `powerSelector`) {
+        power();
     }
-
-//Checking who won and displaying message
-    if (lapsCompleted === 3) {
-        if (userRacer.position > rival.position) {
-            push();
-            textSize(64)
-            textAlign(CENTER);
-            fill(255, 255, 0);
-            text(`You WIN!`, width/2, height/2);
-            pop();
-        }
-        else {
-            push();
-            textSize(48)
-            textAlign(CENTER);
-            fill(255, 255, 0);
-            text(`You Lose`, width/2, height/2);
-            pop();
-        }
-        noLoop();
+    if (state === `angleSelector`) {
+        angle();
     }
+}
+
+//Function for the power bar
+function power() {
+    //Making the rectangle shrink then grow
+    powerBar.height = powerBar.height + powerBar.heightChange;
+
+    if (powerBar.height >= powerBar.maxHeight) {
+        powerBar.heightChange = -powerBar.heightChange;
     }
+    if (powerBar.height <= powerBar.minHeight) {
+        powerBar.heightChange = powerBar.heightChange * -1;
+    }
+    
+    //Mapping the power value
+    powerValue = map(powerBar.height, 0, 400, 0, 100);
 
+    push();
+    fill(255);
+    rect(powerBar.x, powerBar.y, powerBar.width, powerBar.maxHeight);
+    pop();
 
+    push();
+    fill(powerBar.fill.r, powerBar.fill.g, powerBar.fill.b);
+    rect(powerBar.x, powerBar.y, powerBar.width, powerBar.height);
+    pop();
+
+    push();
+    textSize(32);
+     stroke(200, 0, 0);
+    strokeWeight(5);
+    text(round(powerValue), 600, 125);
+    pop();
+}
+
+//Check power function
+function checkPower() {
+    powerBar.heightChange = 0;
+    powerValue = round(powerValue);
+    console.log(powerValue);
+}
+
+//MousePressed
+function mouseClicked() {
+    if (state === `powerSelector`) {
+        checkPower()
+        state = `angleSelector`
+    }
+    if (state === `angleSelector`) {
+        checkAngle()
+    }
+}
+
+//Angle function
+function angle() {
+    //Making the angle change and bounce
+    angleArrow.angle = angleArrow.angle + angleArrow.angleChange;
+
+    if (angleArrow.angle >= angleArrow.maxAngle) {
+        angleArrow.angleChange = -angleArrow.angleChange;
+    }
+    if (angleArrow.angle <= angleArrow.minAngle) {
+        angleArrow.angleChange = angleArrow.angleChange * -1;
+    }
+        
+    // Calculate the arrow endpoint
+    let arrowEndX = angleArrow.centerX + angleArrow.length * cos(radians(angleArrow.angle));
+    let arrowEndY = angleArrow.centerY + angleArrow.length * sin(radians(angleArrow.angle));
+
+    // Draw the arrow
+    push();
+    stroke(angleArrow.fill.r, angleArrow.fill.g, angleArrow.fill.b);
+    strokeWeight(10);
+    line(angleArrow.centerX, angleArrow.centerY, arrowEndX, arrowEndY);
+    pop();
+
+    // Drawing the central point
+    push();
+    fill(0);
+    ellipse(angleArrow.centerX, angleArrow.centerY, angleArrow.centerSize);
+    pop();
+}
+
+//Check angle function
+function checkAngle() {
+    angleArrow.angleChange = 0;
+    angleValue = angleArrow.angle;
+    console.log(angleArrow.angle);
 }
